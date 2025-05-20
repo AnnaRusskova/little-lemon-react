04ad3e0e0f70d188db7172377e6363479ded0ffd
Anna R <anna.russkova@gmail.com>
2025-05-20 17:40:44 -0400

    components_&_booking

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import './App.css';
+import {BrowserRouter} from 'react-router-dom';
+
+import Nav from './components/Nav';
+import Main from './components/Main';
+import Menu from './components/Menu';
+import Footer from './components/Footer';
+
 
 function App() {
   return (
-    <div className="App">
-      <h1>
-        Homepage
-      </h1>
-    </div>
+    <BrowserRouter> {/* Оборачиваем все компоненты в BrowserRouter */}
+      <Nav />
+      <Main />
+      <Menu />
+      <Footer />
+    </BrowserRouter>
   );
 }
 
